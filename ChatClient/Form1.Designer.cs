7333cf675326a4590240b1b127f107ede858fc4c
Fabio Miranda <web.jrhk@gmail.com>
2024-04-24 14:05:01 -0300

    Windows Forms Layout Done

--- ChatClient/Form1.Designer.cs
+++ ChatClient/Form1.Designer.cs
@@ -28,13 +28,139 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtServidorIP = new System.Windows.Forms.TextBox();
+            this.numPortaHost = new System.Windows.Forms.NumericUpDown();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.txtMensagem = new System.Windows.Forms.TextBox();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.labelStatus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numPortaHost)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtServidorIP
+            // 
+            this.txtServidorIP.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtServidorIP.Location = new System.Drawing.Point(17, 16);
+            this.txtServidorIP.Name = "txtServidorIP";
+            this.txtServidorIP.Size = new System.Drawing.Size(446, 38);
+            this.txtServidorIP.TabIndex = 0;
+            this.txtServidorIP.Text = "127.0.0.1";
+            // 
+            // numPortaHost
+            // 
+            this.numPortaHost.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numPortaHost.Location = new System.Drawing.Point(469, 16);
+            this.numPortaHost.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.numPortaHost.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numPortaHost.Name = "numPortaHost";
+            this.numPortaHost.Size = new System.Drawing.Size(233, 38);
+            this.numPortaHost.TabIndex = 1;
+            this.numPortaHost.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuario.Location = new System.Drawing.Point(17, 60);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(446, 38);
+            this.txtUsuario.TabIndex = 2;
+            this.txtUsuario.Text = "Visitante";
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConectar.ForeColor = System.Drawing.Color.Green;
+            this.btnConectar.Location = new System.Drawing.Point(469, 61);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(233, 37);
+            this.btnConectar.TabIndex = 3;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(17, 105);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.Size = new System.Drawing.Size(685, 499);
+            this.txtLog.TabIndex = 4;
+            // 
+            // txtMensagem
+            // 
+            this.txtMensagem.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMensagem.Location = new System.Drawing.Point(17, 610);
+            this.txtMensagem.Name = "txtMensagem";
+            this.txtMensagem.Size = new System.Drawing.Size(446, 38);
+            this.txtMensagem.TabIndex = 5;
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnviar.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btnEnviar.Location = new System.Drawing.Point(469, 610);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(233, 37);
+            this.btnEnviar.TabIndex = 6;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStatus.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.labelStatus.Location = new System.Drawing.Point(17, 662);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(685, 52);
+            this.labelStatus.TabIndex = 7;
+            this.labelStatus.Text = "não conectado";
+            this.labelStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(714, 723);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.txtMensagem);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.numPortaHost);
+            this.Controls.Add(this.txtServidorIP);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numPortaHost)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtServidorIP;
+        private System.Windows.Forms.NumericUpDown numPortaHost;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.TextBox txtMensagem;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.Label labelStatus;
     }
 }
 
